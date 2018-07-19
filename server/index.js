87f9fc13c6d2efd04080fa68a7d9ef95ec627aa8
andrew wong <awwong92@gmail.com>
2018-07-18 23:03:10 -0400

    resolved DB issues interfereing with deployment

--- server/index.js
+++ server/index.js
@@ -23,17 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/auth", require("./auth"));
 app.use("/api", require("./api"));
 
-// any remaining requests with an extension (.js, .css, etc.) send 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error("Not found");
-    err.status = 404;
-    next(err);
-  } else {
-    next();
-  }
-});
-
 app.use("*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
