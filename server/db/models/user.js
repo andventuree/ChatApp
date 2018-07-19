cb74a0f814fa011d21c5642b3eab69d765e2e42d
andrew wong <awwong92@gmail.com>
2018-07-19 01:54:27 -0400

    added more users and improved time stamp

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -7,7 +7,18 @@ const cartoonProfiles = [
   "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/50.png",
   "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/64.png",
   "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/102.png",
-  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/21.png"
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/21.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/83.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/41.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/17.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/83.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/78.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/24.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/42.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/54.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/female/72.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/1.png",
+  "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/22.png"
 ];
 
 const getRandomImg = () =>
--- src/components/TimeStamp.js
+++ src/components/TimeStamp.js
@@ -2,18 +2,36 @@ import React from "react";
 
 export default function TimeStamp(props) {
   const { isoTime } = props;
-  let hours = isoTime.getHours() + 1;
-  let minutes = isoTime.getMinutes() + 1;
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  let month = months[isoTime.getMonth()];
+  let hours = isoTime.getHours();
+  let minutes = isoTime.getMinutes();
+  let day = isoTime.getDate();
   minutes = minutes < 10 ? "0" + minutes : minutes;
   let partOfDay = "AM";
 
-  if (hours === 24) {
+  if (hours === 24 || hours === 0) {
     // 12 AM
     hours = 12;
   } else if (hours >= 12) {
-    if (hours > 12) hours % 12; //anytime in the afternoon
+    hours = hours > 12 ? hours % 12 : hours; //anytime in the afternoon
     partOfDay = "PM";
   }
 
-  return <em className="date">{`${hours}:${minutes} ${partOfDay}`}</em>;
+  return (
+    <em className="date">{`${month} ${day} @ ${hours}:${minutes} ${partOfDay}`}</em>
+  );
 }
