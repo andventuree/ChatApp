18020d74e90bc872330e4b336f6a121c68a64b91
andrew wong <awwong92@gmail.com>
2018-07-29 01:13:10 -0400

    added express session and sequelize connector

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const db = require("./db");
+const sessionStore = new SequelizeStore({ db });
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
@@ -11,6 +14,19 @@ const server = app.listen(PORT, () =>
 
 const io = require("socket.io")(server);
 require("./socket")(io); // Takes in server and handle socket events
+
+app.use(
+  session({
+    secret: "chat-app-secret",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+  }),
+  (req, res, next) => {
+    console.log("req.session: ", req.session);
+    next();
+  }
+);
 
 app.use(morgan("dev"));
 
