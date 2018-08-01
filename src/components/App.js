3fb28324f0a4285183789eba56322da4484c0f47
andrew wong <awwong92@gmail.com>
2018-08-01 02:38:55 -0400

    login renders at root path / since no home page

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <div>
         <Switch>
+          <Route exact path="/" component={Login} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           {isLoggedIn && <Route path="/" component={Main} />}
